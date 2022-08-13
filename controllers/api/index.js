e48c0ece402fc08c5006b7f34bb28be1e13fe5ba
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2022-08-13 18:55:32 -0400

    some more route changes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,6 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes');
-// const gameRoutes = require('./game-routes');
-const reviewRoutes = require('./review-routes');
-
-router.use('/user', userRoutes);
-// router.use('/game', gameRoutes);
-router.use('/review', reviewRoutes);
+const notesRoute = require('./notes');
+router.use('/notes', notesRoute);
 
 module.exports = router;
