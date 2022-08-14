3fc6601e184fefda60ad6bbb0fc2e1561ff04ecd
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2022-08-13 23:15:22 -0400

    changed /api/notes on api route for get

--- controllers/api/notes.js
+++ controllers/api/notes.js
@@ -3,10 +3,11 @@ const fs = require("fs");
 const  route  = require("../homeRoutes");
 
 
- route.get("/api/notes", function (req, res) {
+ route.get("/notes", function (req, res) {
         fs.readFile(__dirname + "/../db/db.json", (err, data) => {
         if (err) throw err;
         res.json(JSON.parse(data));
+        console.log(data);
         });
     });
   
