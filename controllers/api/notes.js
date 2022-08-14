faf2dbdda111ae357e3381ccc35c8cefb3b302f4
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2022-08-13 23:33:05 -0400

    made post request on api route, it logs out request but loads a lot on postman

--- controllers/api/notes.js
+++ controllers/api/notes.js
@@ -1,14 +1,26 @@
 
 const fs = require("fs");
-const  route  = require("../homeRoutes");
+const route = require("../homeRoutes");
 
 
- route.get("/notes", function (req, res) {
-        fs.readFile(__dirname + "/../db/db.json", (err, data) => {
+route.get("/notes", function (req, res) {
+    fs.readFile(__dirname + "/../db/db.json", (err, data) => {
         if (err) throw err;
         res.json(JSON.parse(data));
         console.log(data);
-        });
     });
-  
+});
+
+// I can post in a note on and log it out on the console, but I'm having issues diplaying on page..
+    // I need to create a new array and add the created note
+        // once I do that I have to update the db.json file so on reload the page will display all the updated reviews
+route.post("/notes", function (req, res) {
+    let everyNote = [];
+    let createdNote = {
+        title: req.body.title,
+        text: req.body.text,
+    }
+    console.log(createdNote);
+})
+
 module.exports = route;
